5b88dc2e13d680fafa10f43ec0986979c0beeecc
VIJO33 <vijo6366@gmail.com>
2025-06-08 13:06:12 +0800

    Add run_fingerprint_service.bat 1749359171770

--- run_fingerprint_service.bat
+++ run_fingerprint_service.bat
@@ -1,8 +1,6 @@
 @echo off
 REM Batch script to start the fingerprint service server on port 8001
 
-REM Change directory to the project folder
-cd /d "C:\xampp\htdocs\sdckl-student-attendance-system-main (2)\sdkl-attendance-system-new"
-
-REM Run the fingerprint service Python server
+REM Assuming this script is run from the "sdckl-attendance-system-new" directory
+REM Run the fingerprint service Python server with relative path
 python middleware/fingerprint_service.py
